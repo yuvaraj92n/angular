e3b102b7d59ca07de6c08a528f0e4bef9815cbad
yuvaraj.n <yuvaraj.n@iinterchange.com>
2022-02-05 12:02:20 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
